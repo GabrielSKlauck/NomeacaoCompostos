1cce808289c26d5dfc4989458da666e3db7dfd83
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-03-07 19:08:38 -0300

    Inicio

---
+++ NomeacaoCompostos/NomeacaoCompostos/Nomeacao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomeacaoCompostos
+{
+    public class Nomeacao
+    {
+        public string GetFormulaMolecular(string molecula)
+        {       
+            Console.WriteLine(GetTipoPrefixo(molecula));
+            return null;
+        }
+
+        private int GetTipoPrefixo(string molecula)
+        {
+            
+            string prefixo;
+            String linha = "";
+            try
+            {              
+                StreamReader sr = new StreamReader("./ListaPrefixos.txt");                                 
+                while (linha != null)
+                {                                                        
+                    linha = sr.ReadLine();
+                    prefixo = linha.Substring(0, linha.IndexOf('-'));
+
+                    if (molecula.StartsWith(prefixo))
+                    {                                               
+                        return (int)Int64.Parse(linha.Substring(linha.IndexOf('-')+1));
+                    }
+                }              
+                sr.Close();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }           
+            return 0;
+        }
+    }
+}
---
+++ NomeacaoCompostos/NomeacaoCompostos/Principal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomeacaoCompostos
+{
+    public class Principal
+    {
+        public static void Main(string[] args)
+        {
+            Nomeacao nomeacao = new Nomeacao();
+            Console.WriteLine(nomeacao.GetFormulaMolecular("etano"));
+        }
+    }
+}
